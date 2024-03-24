55f4e3c7fb5ba07105b150cf60ea16cd988acb2d
Ishaan Bedi <bediishaan6@gmail.com>
2024-03-24 16:23:22 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script defer src="https://umami.ishaanbedi.com/script.js" data-website-id="fc181517-b011-4764-89f5-fd178dc06a3f"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
